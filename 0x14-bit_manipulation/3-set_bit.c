10c5f10194ed3d54c5c3a506b4f54e62e719e9d9
esrAA312 <esraayahya312@gmail.com>
2023-09-28 11:14:00 -0700

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -20,18 +20,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	for (i = 0; *n > 0; i++)
-	{
-		a[i] = *n % 2;
-		*n = *n / 2;
+	*n |= (1UL << index);
 
-	}
-
-	a[index] = 1;
-
-	for (t = 0; n[t]; t++)
-
-		va = 2 * va + (n[t] - '0');
-	*n = va;
 	return (1);
 }
