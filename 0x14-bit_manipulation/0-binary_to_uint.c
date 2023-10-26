6eb9475f19194f0a9eb152fdb528e6d90dddb387
esrAA312 <esraayahya312@gmail.com>
2023-10-26 10:21:35 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,28 +12,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i, num;
-	unsigned int rem, x, base = 1, n;
+	int i, x, n;
 	unsigned int int_num = 0;
 
 	if (!b)
-		return (0);
+		return 0;
+
 	x = strlen(b);
-	num = atoi(b);
 
-
-	for (n = 0; b[n]; n++)
-	{
+	for (n = 0; b[n]; n++) {
 		if (b[n] < '0' || b[n] > '1')
-			return (0);
+			return 0;
 	}
 
-	for (i = x - 1; i >= 0; i--)
-	{rem = num % 10;
-		int_num = int_num + rem * base;
-		num = num / 10;
-		base = base * 2;
+	for (i = 0; i < x; i++) {
+		int_num = int_num * 2 + (b[i] - '0');
 	}
-	return (int_num);
+
+	return int_num;
 }
+
 
