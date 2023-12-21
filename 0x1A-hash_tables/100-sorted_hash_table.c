601f2c8ffb546899ca09623aea666e1daf94d151
esrAA312 <esraayahya312@gmail.com>
2023-12-21 11:23:23 -0800

    done

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -201,7 +201,16 @@ void shash_table_delete(shash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		bucket = ht->array[i];
+		while (bucket)
+		{
+			aux_free = bucket;
+			bucket = bucket->next;
+			if (aux_free->key)
+				free(aux_free->key);
+			if (aux_free->value)
+				free(aux_free->value);
+			free(aux_free);
+		}
 	}
-	free(ht->array);
-	free(ht);
+
 }
