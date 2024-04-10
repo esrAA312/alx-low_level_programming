198b475621ebac6c1ed09363b2608c325c0fbad9
esrAA312 <esraayahya312@gmail.com>
2024-04-10 05:47:13 -0700

    done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,7 @@ int jump_search(int *array, size_t size, int value);
 int midpoint(int staes, int enes);
 int interpolation_search(int *array, size_t size, int value);
 size_t calculate_position(int *ar, size_t l, size_t h, int v);
+int exponential_search(int *array, size_t size, int value);
+int mod_binary_search(int *array, int value, int begin, int end);
+
 #endif
